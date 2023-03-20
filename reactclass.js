e8b30d669f756549839c5581d9284db7218ad120
yokgs <yazid.slila01@gmail.com>
2023-03-20 01:17:06 +0000

    + 0.1.0

---
+++ reactclass.js
@@ -0,0 +1,15 @@
+/** 
+ * Generated using DyeScript Light
+ * by Yazid Slila (@yokgs)
+ */
+import { StyleSheet } from "react-native";
+export default StyleSheet.create({
+'maincontent': {
+  'backgroundColor': 'red',
+
+},
+'container': {
+  'border': 0,
+
+}
+});
